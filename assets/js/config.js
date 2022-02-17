8177f92c6f087002b5fe58208b0138cddb1c093e
sohainewbie <sohainewbie666@gmail.com>
2022-02-17 14:46:31 +0700

    init config

---
+++ assets/js/config.js
@@ -0,0 +1,30 @@
+let articles = [
+  {
+      'url' : 'article/writeup-infernoctf2019-web-crackme.html',
+      'title' : 'Writeup infernoctf2019 Web Crackme',
+      'date' : '2019-12-29 11:35:49',
+      'author' : 'sohainewbie',
+      'category': 'ctf, reversing'
+  },
+  {
+      'url' : 'article/writeup-ritsec2019-our-first-api.html',
+      'title' : 'Writeup ritsec2019 Our First API',
+      'date' : '2019-11-18 14:60:49',
+      'author' : 'sohainewbie',
+      'category': 'ctf, web'
+  },
+  {
+      'url' : 'article/writeup-ritsec2019-our-first-api.html',
+      'title' : 'Writeup Single Special FLARE-ON Level for BHUSA2019',
+      'date' : '2019-08-09 05:10:49',
+      'author' : 'sohainewbie',
+      'category': 'ctf, reversing'
+  },
+  {
+      'url' : 'article/writeup-bsidestlv2019-web-pacman-challenges.html',
+      'title' : 'Writeup bsidestlv2019 Web Pacman Challenges',
+      'date' : '2019-07-29 10:08:49',
+      'author' : 'sohainewbie',
+      'category': 'ctf, pwn, web'
+  }
+];
